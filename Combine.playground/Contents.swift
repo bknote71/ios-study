5f169c5e23f73403ca6602c63dd87fe15fa193cb
bknote71 <bknote71@gmail.com>
2024-02-25 22:41:40 +0900

    dropFirst, removeDuplicates in Combine

--- Combine.playground/Contents.swift
+++ Combine.playground/Contents.swift
@@ -29,4 +29,50 @@ age.send(20) // print("age", 20)
 age.send(20) // print("age", 20)
 age.send(21) // print("age", 21)
 
+// dropFirst: 첫 번째 요소 제거
+// removeDuplicates: 중복 요소 제거
 
+// 예) CurrentValueSubject + dropFirst
+// CurrentValueSubject에서 첫 번째 요소를 제거한다는 것은, 최초 sink 호출 시의 현재 값을 publish 하는 것을 처리하지 않는다는 것!
+let count = CurrentValueSubject<Int, Never>(1)
+count
+    .dropFirst()
+    .sink { print("count", $0) }
+
+count.send(2)
+count.send(3)
+
+// 예) CurrentValueSubject + removeDuplicates
+// - 이벤트가 2번 통과해야 호출된다.
+count
+    .dropFirst() // 최초 X
+    .removeDuplicates { // 상세한 조건 정의
+        print("old:", $0, "new:", $1)
+        return $0 == $1
+    }
+    .sink { print("removed duplicates count", $0) }
+
+count.send(4) // 1
+count.send(4) // 2
+count.send(5) // 3
+
+// Q. 여기서의 old value는 removeDuplicates를 통과한 값이겠죠? O
+struct Member: Identifiable {
+    var id: String
+    var name: String?
+    var age: Int?
+
+    static func == (lhs: Member, rhs: Member) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
+
+let members = CurrentValueSubject<Member, Never>(Member(id: "1", name: "bk"))
+members
+    .removeDuplicates {
+        print("old:", $0, "new:", $1)
+        return $0 == $1
+    }
+    .sink { print("member", $0) }
+
+members.send(Member(id: "1", name: "bk2"))
